32bbcda73b509da59aaa13f1f4a634c42e89c775
Grisuno <grisuno@gmail.com>
2024-06-28 00:55:14 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Nombre del entorno virtual
+ENV_NAME="env"
+
+# Ruta del script de Python
+SCRIPT_NAME="cli.py"
+
+# Crear un entorno virtual
+python3 -m venv $ENV_NAME
+
+# Activar el entorno virtual
+source $ENV_NAME/bin/activate
+
+# Actualizar pip
+pip install --upgrade pip
+
+# Instalar las bibliotecas necesarias
+pip install opencv-python Pillow moviepy
+
+# Ejecutar el script de Python pasando el prompt de texto como parámetro del video que se quiere generar
+python $SCRIPT_NAME "$1"
+
+# Desactivar el entorno virtual
+deactivate
